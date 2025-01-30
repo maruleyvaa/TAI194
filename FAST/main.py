289e4987689b27c93b3b05a1f16909cbd2a08d3f
Maria Eugenia Leyva Avila <122042647@upq.edu.mx>
2025-01-29 18:08:19 -0600

    Practica 2

--- FAST/main.py
+++ FAST/main.py
@@ -8,4 +8,15 @@ app= FastAPI() #todo se hará a través de este objeto
 def home(): #funcion que se ejecutará cuando se entre a la ruta
     return {"hello": "world fastApi"}#mensaje que se mostrará en la ruta
 
+#crear segunda ruta o EndPoint
+@app.get("/promedio")#declarar ruta del servidor
 
+def promedio(): #funcion que se ejecutará cuando se entre a la ruta
+    return 10.5
+
+#crear tercer ruta o endpoint con parametro obligatorio
+@app.get("/usuario/{id}")#declarar ruta del servidor
+
+def consultausuario(id:int): #funcion que se ejecutará cuando se entre a la ruta
+    #caso ficticio de consulta a base de datos
+    return {"Se encontró el usuario": id}
